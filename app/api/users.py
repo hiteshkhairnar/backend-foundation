7baf5eadc04be50ea7a9bbae010c010674970a0a
Hitesh Khairnar <hiteshkhairnar503@gmail.com>
2026-07-15 19:59:49 +0530

    Day 6 - Implemented Update and Delete APIs

--- app/api/users.py
+++ app/api/users.py
@@ -3,7 +3,12 @@ from sqlalchemy.orm import Session
 
 from app.database.database import get_db
 from app.schemas.user import UserCreate, UserResponse
-from app.services.user_service import create_user, get_users
+from app.services.user_service import (
+    create_user,
+    get_users,
+    update_user,
+    delete_user
+)
 
 router = APIRouter(
     prefix="/users",
@@ -24,3 +29,31 @@ def read_users(
     db: Session = Depends(get_db)
 ):
     return get_users(db)
+
+@router.put("/{user_id}", response_model=UserResponse)
+def update_existing_user(
+    user_id: int,
+    user: UserCreate,
+    db: Session = Depends(get_db)
+):
+    updated_user = update_user(db, user_id, user)
+
+    if not updated_user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    return updated_user
+
+
+@router.delete("/{user_id}")
+def delete_existing_user(
+    user_id: int,
+    db: Session = Depends(get_db)
+):
+    deleted_user = delete_user(db, user_id)
+
+    if not deleted_user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    return {
+        "message": "User deleted successfully"
+    }
--- app/services/user_service.py
+++ app/services/user_service.py
@@ -18,3 +18,32 @@ def create_user(db: Session, user: UserCreate):
 
 def get_users(db: Session):
     return db.query(User).all()
+
+def get_user(db: Session, user_id: int):
+    return db.query(User).filter(User.id == user_id).first()
+
+
+def update_user(db: Session, user_id: int, user: UserCreate):
+    db_user = get_user(db, user_id)
+
+    if not db_user:
+        return None
+
+    db_user.name = user.name
+    db_user.email = user.email
+
+    db.commit()
+    db.refresh(db_user)
+
+    return db_user
+
+def delete_user(db: Session, user_id: int):
+    db_user = get_user(db, user_id)
+
+    if not db_user:
+        return None
+
+    db.delete(db_user)
+    db.commit()
+
+    return db_user
